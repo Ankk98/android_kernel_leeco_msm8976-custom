3f525ac68def273e7886cfc5c1fa7527a16d9f49
ankk98 <ankk98@gmail.com>
2018-03-27 03:16:55 +0530

    initial commit

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Mar 16 21:18:32 IST 2018"
+#define LINUX_COMPILE_BY "ankk98"
+#define LINUX_COMPILE_HOST "ankk98-linux"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
